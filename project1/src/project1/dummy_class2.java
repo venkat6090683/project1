f156dd02ae79cd89bf1f08ca030e488bc6fa4106
srelaxmi <sreem.ece@gmail.com>
2023-09-12 17:55:01 +0530

    changed venkat to seelam  in class

--- project1/src/project1/dummy_class2.java
+++ project1/src/project1/dummy_class2.java
@@ -3,6 +3,6 @@ package project1;
 public class dummy_class2 {
  public static void main(String[] args) {
 	System.out.println("sai kumar");
-	System.out.println("venkat");
+	System.out.println("seelam");
 }
 }
